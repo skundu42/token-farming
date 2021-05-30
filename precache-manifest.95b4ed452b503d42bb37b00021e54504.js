57975a1cc93ab89e656baecad1d041fdbc680846
skundu42 <skundu_be19@thapar.edu>
2021-05-30 19:25:16 +0530

    Updates

---
+++ precache-manifest.95b4ed452b503d42bb37b00021e54504.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "623770482338f08db274",
+    "url": "/token-farming/static/css/main.cc281677.chunk.css"
+  },
+  {
+    "revision": "623770482338f08db274",
+    "url": "/token-farming/static/js/main.62377048.chunk.js"
+  },
+  {
+    "revision": "05ca0e3a68e7b87ee91f",
+    "url": "/token-farming/static/css/1.56cf168f.chunk.css"
+  },
+  {
+    "revision": "05ca0e3a68e7b87ee91f",
+    "url": "/token-farming/static/js/1.05ca0e3a.chunk.js"
+  },
+  {
+    "revision": "ad72a11cca6fb648347d",
+    "url": "/token-farming/static/js/runtime~main.ad72a11c.js"
+  },
+  {
+    "revision": "559db61a846bbe8567e8faee3581eb4b",
+    "url": "/token-farming/index.html"
+  }
+];
